ef615ab20227967bf9b83526444a5c19022bc84c
Vijayant Prateek <prateekviju1770@gmail.com>
2022-10-28 22:54:50 +0530

    Backend to frontend data sending

--- src/app/services/user-data-from-home.service.ts
+++ src/app/services/user-data-from-home.service.ts
@@ -16,14 +16,14 @@ export class UserDataFromHomeService {
   userServiceData:any = [{}];
   constructor() {
     this.socket = io('http://localhost:5009');
-      // console.log("Socket", this.socket);
+    // console.log("Socket", this.socket);
    }
 
   setUserDataInService(data:any){
     this.userServiceData = data;
     console.log("Service Data : ", this.userServiceData);
-    // let temp = this.socket?.emit("new-user",this.userServiceData);
 
+    //sending data to backend
     this.socket.emit("new-user", this.userServiceData);
 
     // Data from backend and modified
